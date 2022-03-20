e9026babdba7b1aa789948b5b0c7fdcdedd3b1a6
Ravi Shukla <rshuklabbb@gmail.com>
2022-03-20 17:53:55 +0530

    feat: remove resume page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import Preloader from "../src/components/Pre";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
 import Footer from "./components/Footer";
-import Resume from "./components/Resume/ResumeNew";
 import {
   BrowserRouter as Router,
   Route,
   Routes,
 } from "react-router-dom";
-import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -30,9 +28,7 @@ function App() {
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
         <Navbar />
-        <ScrollToTop />
         <Routes>
-          <Route path="/resume" exact element={<Resume />} />
           <Route path="/:id" exact element={<Home />} />
           <Route path="*" element={<Home />} />
         </Routes>
