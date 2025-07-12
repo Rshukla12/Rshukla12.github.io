599bc4e7f70b5c9d00817e91a18b054f89161549
Ravi <Personal@Ravis-MacBook-Pro.local>
2025-07-12 17:57:43 +0530

    fix: retrigger deployment

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/linkedin-profile.jpg";
+import myImg from "../../Assets/profile.jpg";
 
 function Home2() {
   return (
