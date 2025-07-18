483fa1cd9595ecf0a00c38f229ff013f5bfeb7f5
Ravi <Personal@Ravis-MacBook-Pro.local>
2025-07-19 00:40:11 +0530

    fixes: unused variable

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useRef } from "react";
 import { Container } from "react-bootstrap";
 import { ImPointRight } from "react-icons/im";
 
--- src/components/Skills/Techstack.js
+++ src/components/Skills/Techstack.js
@@ -2,17 +2,13 @@ import React from "react";
 import { Col, Row } from "react-bootstrap";
 import {
   DiJavascript1,
-  DiReact,
   DiNodejs,
   DiMongodb,
   DiPython,
-  DiJava,
-  DiHtml5,
-  DiCss3Full
+  DiJava
 } from "react-icons/di";
 
 import {
-  SiMaterialui,
   SiExpress,
   SiApachekafka,
   SiRedis,
